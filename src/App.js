9ca04505ada61967ab7dc0d703c257905740a0dc
Sreejay123 <105335086+Sreejay123@users.noreply.github.com>
2023-03-21 16:59:47 +0530

    Netflix Clone using react
    A simple project that is built using react and TMDb API.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import './App.css'
+import Banner from './Components/Banner/Banner.js'
+import RowPost from './Components/RowPost/RowPost'
+import {originals,action, documentary, romance, horror, comedy} from './Urls'
+
+function App() {
+  return (
+    <div className='app'>
+      <NavBar/>
+      <Banner/>
+      <RowPost title="Netflix Originals"  url= {originals}/>
+      <RowPost title="Action" url={action} isSmall/>
+      <RowPost title="Documentary" url={documentary} isSmall/>
+      <RowPost title="Romance" url={romance} isSmall/>
+      <RowPost title="Horror" url={horror} isSmall/>
+      <RowPost title="Comedy" url={comedy} isSmall/>
+    </div>
+  )
+}
+
+export default App;
